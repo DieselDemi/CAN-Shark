e1a68c88cab5247989f7fa123e69095045ab44a0
Demetry <demetryromanowski@gmail.com>
2022-05-01 14:51:23 -0400

    Initial code for the can shark driver

---
+++ libcanshark/LibCanShark.h
@@ -0,0 +1,29 @@
+#ifndef XDFCREATOR_LIBCANSHARK_H
+#define XDFCREATOR_LIBCANSHARK_H
+
+#include <QThread>
+#include <QMutex>
+
+class LibCanShark : public QThread {
+    explicit LibCanShark(QObject *parent = nullptr);
+    ~LibCanShark();
+
+    void connect(const QString &portName, int waitTimeout, const QString& response);
+
+    void run() Q_DECL_OVERRIDE;
+
+signals:
+    void request(const QString& s);
+    void error(const QString& s);
+    void timeout(const QString& s);
+
+private:
+    QString portname;
+    QString response;
+    int waitTimeout;
+    QMutex mutex;
+    bool quit;
+};
+
+
+#endif //XDFCREATOR_LIBCANSHARK_H
--- libcanshark/libcanshark.cpp
+++ libcanshark/libcanshark.cpp
@@ -1,1 +1,29 @@
+#include "LibCanShark.h"
 
+LibCanShark::LibCanShark(QObject *parent) : QThread(parent) {
+
+}
+
+LibCanShark::~LibCanShark() {
+
+}
+
+void LibCanShark::connect(const QString &portName, int waitTimeout, const QString &response) {
+
+}
+
+void LibCanShark::run() {
+    QThread::run();
+}
+
+void LibCanShark::request(const QString &s) {
+
+}
+
+void LibCanShark::error(const QString &s) {
+
+}
+
+void LibCanShark::timeout(const QString &s) {
+
+}
